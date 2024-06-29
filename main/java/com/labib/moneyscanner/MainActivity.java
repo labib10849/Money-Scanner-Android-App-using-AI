cda864f32ae459933797b65acc1f5afd05ac3ec8
labib10849 <154999586+labib10849@users.noreply.github.com>
2024-06-29 21:51:12 +0600

    Add files via upload

---
+++ main/java/com/labib/moneyscanner/MainActivity.java
@@ -0,0 +1,26 @@
+package com.labib.moneyscanner;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.labib.moneyscanner.DetectionActivity;
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DetectionActivity.class));
+            }
+        });
+    }
+}
